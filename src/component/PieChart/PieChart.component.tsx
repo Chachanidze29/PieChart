a5ae7f438b45cbdae0bbaecb3eb2c1c1f9a2acbc
Chachanidze29 <chachanidze29m@gmail.com>
2023-08-26 22:02:41 +0400

    finished logic, working on styles

--- src/component/PieChart/PieChart.component.tsx
+++ src/component/PieChart/PieChart.component.tsx
@@ -1,11 +1,45 @@
 import { useContext } from "../context/context";
 
+import './PieChart.style.css';
+
+const colors = [
+    '#FF5733',
+    '#2ECC71',
+    '#3498DB',
+    '#E74C3C',
+    '#9B59B6'
+];
+
 const PieChart = () => {
     const { items } = useContext();
+    const totalPercentage = items.reduce((total, item) => total + item.itemPercentage, 0);
+
+    let startAngle = 0;
 
     return (
-        <h1>PieChart isn't implemented yet</h1>
-    )
+        <div className="PieChart">
+            {items.map((item, index) => {
+                const sliceAngle = (item.itemPercentage / totalPercentage) * 360;
+                const sliceStyle = {
+                    transform: `rotate(${startAngle}deg)`,
+                    background: colors[index],
+                };
+
+                startAngle += sliceAngle;
+
+                return (
+                    <div
+                        key={index}
+                        className="PieSlice"
+                        style={{
+                            ...sliceStyle,
+                            clipPath: `polygon(50% 50%, 100% 100%, 100% 0%)`,
+                        }}
+                    ></div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default PieChart;
--- src/component/reducer/PieChart.reducer.tsx
+++ src/component/reducer/PieChart.reducer.tsx
@@ -1,4 +1,3 @@
-export const initialItems = [];
 
 export type PieChartItem = {
     id: number
@@ -22,13 +21,27 @@ export type RemoveAction = {
 
 export type Action = AddAction | RemoveAction;
 
+export const initialItems: PieChartItem[] = [];
+
 export const pieChartReducer = (items: PieChartItem[], action: Action) => {
     switch (action.type) {
         case 'add': {
+            const { item } = action.payload;
+
+            const totalPercentage = items.reduce((total, item) => total + item.itemPercentage, 0) + item.itemPercentage;
+            if (totalPercentage > 100) {
+                alert("Sum of percentages shouldn't be more than 100");
+                return items;
+            }
+
+            if (items.length > 5) {
+                return items;
+            }
+
             return [...items, {
-                id: action.payload.item.id,
-                itemName: action.payload.item.itemName,
-                itemPercentage: action.payload.item.itemPercentage
+                id: item.id,
+                itemName: item.itemName,
+                itemPercentage: item.itemPercentage
             }]
         }
         case 'remove': {
