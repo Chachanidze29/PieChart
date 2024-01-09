6db022ffcc9ede8717ff9e7e1ee3c04699685efd
Chachanidze29 <chachanidze29m@gmail.com>
2024-01-09 15:25:05 +0400

    add functionality to show/hide items list dropdown

--- src/component/ItemsList/ItemsList.component.tsx
+++ src/component/ItemsList/ItemsList.component.tsx
@@ -2,26 +2,47 @@ import "./ItemsList.style.css";
 import { useContext } from "../context/context";
 import { PieChartItem } from "../reducer/PieChart.reducer";
 import Item from "../Item/Item.component";
+import { useState } from "react";
 
 const ItemsList = () => {
+  const [showItems, setShowItems] = useState(false);
+
   const { items } = useContext();
 
+  const toggleDropDown = () => {
+    setShowItems((prevState) => !prevState);
+  };
+
   return (
-    <div className="ItemsList">
-      {items.length !== 0 && (
-        <div className="ItemsWrapper">
-          {items.map((item: PieChartItem, index: number) => (
-            <Item
-              key={item.id}
-              id={item.id}
-              index={String(index)}
-              itemName={item.itemName}
-              itemPercentage={item.itemPercentage}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    items.length > 0 && (
+      <div className="ItemsList">
+        {showItems ? (
+          <div className="ArrowsWrapper">
+            <h2>Hide Items</h2>
+            <i onClick={toggleDropDown} className="UpArrow" />
+          </div>
+        ) : (
+          <div className="ArrowsWrapper">
+            <h2>Show Items</h2>
+            <i onClick={toggleDropDown} className="DownArrow" />
+          </div>
+        )}
+
+        {showItems && (
+          <div className="ItemsWrapper">
+            {items.map((item: PieChartItem, index: number) => (
+              <Item
+                key={item.id}
+                id={item.id}
+                index={String(index)}
+                itemName={item.itemName}
+                itemPercentage={item.itemPercentage}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    )
   );
 };
 
